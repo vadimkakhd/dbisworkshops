470f015ee5c6c8b9eb0884ae8b88359cc5bdb793
Vadim Khodakivskyy <vadim_98@ukr.net>
2018-12-25 20:14:46 +0200

    update_2

--- km-52/Khodakivskyi_Vadym/project/Code/populate.sql
+++ km-52/Khodakivskyi_Vadym/project/Code/populate.sql
@@ -1,10 +1,10 @@
 INSERT INTO "User"
 (LOGIN, PASS, NICKNAME, NAME, FACULTY_NAME, COURSE_NUMBER)VALUES
-('vadya', 'vadya', 'vadimka', 'Vadym', 'Applied mathematics', 4);
+('vadya', 'vadya9', 'vadimka', 'Vadym', 'Applied mathematics', 4);
 
 INSERT INTO "User"
 (LOGIN, PASS, NICKNAME, SURNAME, FACULTY_NAME, COURSE_NUMBER)VALUES
-('petua228', '234', 'Petya', 'Petrov', 'Applied mathematics', 3);
+('petua228', '234567', 'Petya', 'Petrov', 'Applied mathematics', 3);
 
 INSERT INTO "User"
 (LOGIN, PASS, NICKNAME, FACULTY_NAME, COURSE_NUMBER)VALUES
@@ -39,36 +39,36 @@ INSERT INTO LECTION_THEME
 (LECTION_NAME) VALUES ('Numerical methods');
 
 INSERT INTO user_has_theme
-(user_theme_id, Author_nickname, LECTION_NAME, user_nickname, "Date") 
-values (1, 'vadimka','Math logarithm', 'vadimka', TO_DATE('2005/05/03', 'yyyy/mm/dd'));
+(user_theme_id, Author_nickname, LECTION_NAME,  "Date") 
+values (1, 'vadimka','Math logarithm', TO_DATE('2005/05/03', 'yyyy/mm/dd'));
 
 INSERT INTO user_has_theme
-(user_theme_id, Author_nickname,LECTION_NAME, user_nickname, "Date") 
-values (2, 'Petya','Integration methods', 'valeron', TO_DATE('2005/05/04', 'yyyy/mm/dd'));
+(user_theme_id, Author_nickname, LECTION_NAME,  "Date")  
+values (2, 'Petya','Integration methods', TO_DATE('2005/05/04', 'yyyy/mm/dd'));
 
 INSERT INTO user_has_theme
-(user_theme_id, Author_nickname,LECTION_NAME, user_nickname,"Date") 
-values (3, 'flaffy','Integration methods', 'Petya', TO_DATE('2005/05/05', 'yyyy/mm/dd'));
+(user_theme_id, Author_nickname, LECTION_NAME,  "Date") 
+values (3, 'flaffy','Integration methods', TO_DATE('2005/05/05', 'yyyy/mm/dd'));
 
 INSERT INTO user_has_theme
-(user_theme_id, Author_nickname,LECTION_NAME, user_nickname,"Date") 
-values (4, 'valeron','Programming on Python', 'flaffy', TO_DATE('2005/05/07', 'yyyy/mm/dd'));
+(user_theme_id, Author_nickname, LECTION_NAME,  "Date") 
+values (4, 'valeron','Programming on Python', TO_DATE('2005/05/07', 'yyyy/mm/dd'));
 
 INSERT INTO user_has_resourses
-(user_resourse_id, Author_nickname, resource_name, user_nickname,"Date") 
-values (1, 'vadimka','www.pidruchniki.ua', 'vadimka', TO_DATE('2005/05/03', 'yyyy/mm/dd'));
+(user_resourse_id, Author_nickname, resource_name, "Date") 
+values (1, 'vadimka','www.pidruchniki.ua', TO_DATE('2005/05/03', 'yyyy/mm/dd'));
 
 INSERT INTO user_has_resourses
-(user_resourse_id, Author_nickname, resource_name, user_nickname,"Date") 
-values (2, 'Petya','www.wikipedia.ua', 'valeron', TO_DATE('2005/05/04', 'yyyy/mm/dd'));
+(user_resourse_id, Author_nickname, resource_name, "Date") 
+values (2, 'Petya','www.wikipedia.ua', TO_DATE('2005/05/04', 'yyyy/mm/dd'));
 
 INSERT INTO user_has_resourses
-(user_resourse_id, Author_nickname, resource_name, user_nickname,"Date") 
-values (3, 'flaffy','www.studpedia.ua', 'Petya', TO_DATE('2005/05/05', 'yyyy/mm/dd'));
+(user_resourse_id, Author_nickname, resource_name, "Date")
+values (3, 'flaffy','www.studpedia.ua', TO_DATE('2005/05/05', 'yyyy/mm/dd'));
 
 INSERT INTO user_has_resourses
-(user_resourse_id, Author_nickname, resource_name, user_nickname,"Date") 
-values (4, 'valeron','www.studpedia.ua', 'flaffy', TO_DATE('2005/05/07', 'yyyy/mm/dd'));
+(user_resourse_id, Author_nickname, resource_name, "Date")
+values (4, 'valeron','www.studpedia.ua', TO_DATE('2005/05/07', 'yyyy/mm/dd'));
 
 INSERT INTO INFORMATION
 (INFO_ID,NICKNAME,LECTION_NAME,RESOURCE_NAME,INFORMATION_LINK, "Date") 
